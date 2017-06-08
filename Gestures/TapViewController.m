1b1b09b79b455fd6c5580950f70a9ec1d6642f6f
Alex Lee <x.lee@mail.utoronto.ca>
2017-06-08 13:25:26 -0700

    Tap screen functioning, toggles view's background color on tap

--- Gestures/TapViewController.m
+++ Gestures/TapViewController.m
@@ -14,6 +14,25 @@
 
 @implementation TapViewController
 
+- (void)viewDidLoad;
+{
+    CGFloat width = 100;
+    CGRect frame = CGRectMake(CGRectGetMidX(self.view.bounds) - width/2, CGRectGetMidY(self.view.bounds) - width/2, width, width);
+    
+    UIView *view = [[UIView alloc] initWithFrame:frame];
+    
+    view.backgroundColor = [UIColor orangeColor];
+    
+    [self.view addSubview:view];
+    
+    UITapGestureRecognizer *tapGesture = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(tapHandler:)];
+    
+    [view addGestureRecognizer:tapGesture];
+}
 
+- (void)tapHandler:(UITapGestureRecognizer *)sender;
+{
+    sender.view.backgroundColor = ([sender.view.backgroundColor isEqual:[UIColor purpleColor]]) ? [UIColor orangeColor] : [UIColor purpleColor];
+}
 
 @end
